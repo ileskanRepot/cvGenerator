4b837be4b08f8a62dfd00f9657d721a1b68c7229
Ileska <ileska@tutanota.com>
2024-01-19 19:09:56 +0200

    Make "writeSplittedText" function and use it in every text writing

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ from reportlab.graphics import shapes
 import json
 import os
 
-def drawLeftSideBar(cc, width, color):
-	cc.setFillColor(color)
-	cc.rect(0, 0, width, A4[1], stroke = 0, fill = 1)
-
 def getStringWithCorrectWidth(string, size, startW, endW, font):
 	wholeWidtn = stringWidth(string, font, size)
 	if wholeWidtn < endW - startW:
@@ -31,6 +27,27 @@ def getStringWithCorrectWidth(string, size, startW, endW, font):
 
 	return ret
 
+def writeSplittedText(cc, textArea, font, fontSize, startHor, endHor, startVert):
+	height = A4[1]
+	cc.setFont(font, fontSize)
+
+	text = textArea["value"]
+
+	splittedText = getStringWithCorrectWidth(text, fontSize, startHor, endHor, font)
+	offset = startVert
+	for line in splittedText:
+		cc.drawString(startHor, height - offset, line)
+		offset += fontSize
+
+	if textArea["type"] == "link":
+		cc.linkURL(textArea["link"], (startHor, height - offset + fontSize, endHor, height - startVert + fontSize))
+
+	return offset
+
+def drawLeftSideBar(cc, width, color):
+	cc.setFillColor(color)
+	cc.rect(0, 0, width, A4[1], stroke = 0, fill = 1)
+
 def writeSideBarText(cc, sideWidth, data):
 	cc.setFillColor(colors.white)
 	
@@ -42,28 +59,22 @@ def writeSideBarText(cc, sideWidth, data):
 	textFontSize = 11
 
 	for ii, elem in enumerate(data["cv"]["sideBar"]):
-		cc.setFont("Helvetica-Bold", 13)
-		cc.drawString(paddingLeft, height - offset, elem["title"])
-		offset += 20
+		offset = writeSplittedText(cc, elem["title"], "Helvetica-Bold", 13, paddingLeft, sideWidth - paddingRight, offset)
+		offset += 8
 
 		for value in elem["values"]:
 			cc.setFont("Helvetica", 11)
-
-			splitted = getStringWithCorrectWidth(value["value"], textFontSize, paddingLeft, sideWidth - paddingRight, "Helvetica")
-			offsetStart = offset
-			for text in splitted:
-				cc.drawString(paddingLeft, height - offset, text)
-				offset += 10
-
-			if value["type"] == "link":
-				cc.linkURL(value["link"], (paddingLeft, height - offset + textFontSize, sideWidth - paddingRight, height - offsetStart + textFontSize))
-
+			offset = writeSplittedText(cc, value, "Helvetica", textFontSize, paddingLeft, sideWidth - paddingRight, offset)
 			offset += 5
 		offset += 20
 
 def writeDetails(cc, sidebarWidth, data, paddingLeft, paddingRight):
 	nameFontSize = 25
+	nameFont = "Helvetica-Bold"
+
 	professionFontSize = 12
+	professionFont = "Helvetica"
+
 	locationFontSize = 10
 	details = data["cv"]["mainPage"]["details"]
 
@@ -71,23 +82,17 @@ def writeDetails(cc, sidebarWidth, data, paddingLeft, paddingRight):
 
 	height = A4[1]
 
-	offset = 20
+	offset = nameFontSize + 20
 
 	# NAME
-	nameSplitted = getStringWithCorrectWidth(details["name"], nameFontSize, sidebarWidth + paddingLeft, A4[0] - paddingRight, "Helvetica-Bold")
-	for line in nameSplitted:
-		cc.drawString(sidebarWidth + paddingLeft, height - offset - nameFontSize, line)
-		offset += 20
+	offset = writeSplittedText(cc, details["name"], nameFont, nameFontSize, sidebarWidth + paddingLeft, A4[0] - paddingRight, offset)
+
+	offset -= 5
 
 	# PROFESSION
-	cc.setFont("Helvetica", professionFontSize)
-	professionSplitted = getStringWithCorrectWidth(details["profession"], nameFontSize, sidebarWidth + paddingLeft, A4[0] - paddingRight, "Helvetica-Bold")
-	for line in professionSplitted:
-		cc.drawString(sidebarWidth + paddingLeft, height - offset - nameFontSize, line)
-		offset += 15
-
+	offset = writeSplittedText(cc, details["profession"], professionFont, professionFontSize, sidebarWidth + paddingLeft, A4[0] - paddingRight, offset)
+	
 	offset += 25
-
 	return offset
 
 def drawBackgroundDetails(cc, sidebarWidth, data, paddingLeft, paddingRight, startOffset):
@@ -115,42 +120,30 @@ def drawBackgroundDetails(cc, sidebarWidth, data, paddingLeft, paddingRight, sta
 	
 	for elem in backgroundDetails:
 		cc.setFont(titleFont, titleFontSize)
-		titleSplitted = getStringWithCorrectWidth(elem["title"], titleFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, titleFont)
 		# TITLE
-		for line in titleSplitted:
-			cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - titleFontSize, line)
-			offset += titleFontSize + 5
+		# offset += 5
+		offset = writeSplittedText(cc, elem["title"], titleFont, titleFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, offset)
+		# offset -= 13
 
 		for value in elem["values"]:
 			# PLACE
-			cc.setFont(placeFont, placeFontSize)
-			placeSplitted = getStringWithCorrectWidth(value["place"], placeFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, placeFont)
-			for line in placeSplitted:
-				cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - titleFontSize, line)
-				offset += placeFontSize
-			offset += 10
+			offset += 8
+			offset = writeSplittedText(cc, value["place"], placeFont, placeFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, offset)
 
 			# LABEL
-			cc.setFont(labelFont, labelFontSize)
-			labelSplitted = getStringWithCorrectWidth(value["label"], labelFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, labelFont)
-			for line in labelSplitted:
-				cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - labelFontSize, line)
-				offset += labelFontSize + 5
-
+			offset = writeSplittedText(cc, value["label"], labelFont, labelFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, offset)
+			
 			# TIME
-			cc.setFont(timeFont, timeFontSize)
-			time = "(" + value["start"] + " - " + value["end"] + ")"
-			labelSplitted = getStringWithCorrectWidth(time, timeFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, timeFont)
-			for line in labelSplitted:
-				cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - timeFontSize, line)
-				offset += timeFontSize + 5
+			offset = writeSplittedText(cc, value["duration"], timeFont, timeFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, offset)
+			# time = "(" + value["start"] + " - " + value["end"] + ")"
+			# labelSplitted = getStringWithCorrectWidth(time, timeFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, timeFont)
+			# for line in labelSplitted:
+				# cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - timeFontSize, line)
+				# offset += timeFontSize + 5
 
+			offset += 8
 			# DESCRIPTION
-			cc.setFont(descFont, descFontSize)
-			descSplitted = getStringWithCorrectWidth(value["description"], descFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, descFont)
-			for line in descSplitted:
-				cc.drawString(sidebarWidth + paddingLeft, pageHeight - offset - descFontSize, line)
-				offset += descFontSize + 2
+			offset = writeSplittedText(cc, value["description"], descFont, descFontSize, sidebarWidth + paddingLeft, pageWidth - paddingRight, offset)
 
 			offset += 10
 		offset += 20
@@ -166,6 +159,7 @@ def drawMainPage(cc, sidebarWidth, data):
 
 def createPdf(data):
 	cc = canvas.Canvas("cvGenerated.pdf", pagesize=A4)
+	cc.setTitle("My CV")
 
 	sidebarWidth = 200
 
@@ -174,10 +168,7 @@ def createPdf(data):
 	drawLeftSideBar(cc, sidebarWidth, colors.darkslategray)
 	writeSideBarText(cc, sidebarWidth, data)
 	drawMainPage(cc, sidebarWidth, data)
-	# dd = shapes.Drawing(100, 100)
-	# rr = shapes.Rect(0, 0, 100, 100, fillColor=colors.purple, strokeColor=colors.black)
-	# dd.add(rr)
-
+	
 	cc.save()
 
 def readJson(fname):
